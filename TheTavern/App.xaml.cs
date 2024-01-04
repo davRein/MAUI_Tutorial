f0103c78e4176550a2ce6762feca23e27569783f
d.reinerts <d@dreinerts.de>
2024-01-04 20:28:21 +0100

    Add project files.

---
+++ TheTavern/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TheTavern {
+    public partial class App : Application {
+        public App() {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TheTavern/AppShell.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace TheTavern {
+    public partial class AppShell : Shell {
+        public AppShell() {
+            InitializeComponent();
+        }
+    }
+}
